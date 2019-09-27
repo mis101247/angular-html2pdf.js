51266be9a8aa92696fd513690930d34a8e466c26
Keyo <keyo@aiii.ai>
2019-09-28 00:56:00 +0800

    feat: import html2pdf && use it.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import * as html2pdf from 'html2pdf.js';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'htmlToPdf';
+
+  downloadPdf() {
+    const element = document.body;
+    const opt = {
+      margin: 1,
+      filename: 'myfile.pdf',
+      image: { type: 'png' },
+      html2canvas: { scale: 2 },
+      jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+    };
+    html2pdf(element, opt);
+  }
 }
